e4f7fd11d7eebf345142115b4db3fc0b49cef506
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2021-01-28 17:12:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'debugging-app';
+  // public var1:any;
+  // public var2:any;
+  // public total:any;
+  // public doSum()
+  // {
+  //   // console.log(typeof(this.var1))
+  //   this.total= this.var1 + this.var2;
+  // }
+
+  // stateflag=false;
+  // toggleState(){
+  //   this.stateflag=!this.stateflag;
+  // }
+  // submit(){
+  //   console.log("button submitted");
+  // }
+  // calculateClasses(){
+  //   return{
+  //     btn:true,
+  //     'btn-primary':true,
+  //     'btn-extra-class':this.stateflag
+  //   }
+  // }
+
+  //ngclass
+  isAvailable=true;
+
+  //ngStyle
+  size=10;
+ public color:any
+ 
+
+
+
+
 }
